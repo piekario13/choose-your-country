2eae0ee6da77096c0ab57f82891de1306d4f15f1
Piekarski Jacek <jpiekarski@M1402.local>
2017-11-24 16:41:30 +0100

    ver0

---
+++ script.js
@@ -0,0 +1,42 @@
+// var url = 'https://restcountries.eu/rest/v1/name/';
+// var countriesList = $('#countries');
+// $('#search').click(searchCountries);
+// function searchCountries() {
+//     var countryName = $('#country-name').val();
+// if(!countryName.length) countryName = 'Poland';
+// $.ajax({
+//          url: url + countryName,
+//          method: 'GET',
+//          success: showCountriesList
+//      });
+//      function showCountriesList(resp) {
+//         countriesList.empty();
+//         resp.forEach(function(item) {
+//             $('<li>').text(item.name).appendTo(countriesList);
+//         });
+//     }
+// }
+
+
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
+$('#search').click(searchCountries);
+function searchCountries() {
+    var countryName = $('#country-name').val();
+if(!countryName.length) countryName = 'Poland';
+$.ajax({
+         url: url + countryName,
+         method: 'GET',
+         success: showCountriesList
+     });
+     function showCountriesList(resp) {
+        countriesList.empty();
+        resp.forEach(function(item) {
+            $('<li id="name">').text(item.name).appendTo(countriesList);
+            $('<li id="capital">').text(item.capital).appendTo(countriesList);
+            $('<li id="region">').text(item.region).appendTo(countriesList);
+            $('<li id="population">').text(item.population).appendTo(countriesList);
+            $('<li id="currencies">').text(item.currencies).appendTo(countriesList);
+        });
+    }
+}
